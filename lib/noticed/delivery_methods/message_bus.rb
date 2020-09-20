4e0ee7321eac386fecaf55313f0bb94e01ee781b
Simon Chiu <simononstartups@gmail.com>
2020-09-20 12:40:12 -0400

    Add Message Bus as a delivery method

---
+++ lib/noticed/delivery_methods/message_bus.rb
@@ -0,0 +1,61 @@
+module Noticed
+  module DeliveryMethods
+    class MessageBus < Base
+
+      def deliver
+        MessageBus.publish "/#{channel}", format, client_ids: client_ids, user_ids: user_ids, group_ids: group_ids
+      end
+
+      private
+
+      def format
+        if (method = options[:format])
+          notification.send(method)
+        else
+          notification.params
+        end
+      end
+
+      def client_ids
+        if (method = options[:client_ids])
+          notification.send(method)
+        else
+          []
+        end
+      end
+
+      def user_ids
+        if (method = options[:user_ids])
+          notification.send(method)
+        else
+          []
+        end
+      end
+
+      def group_ids
+        if (method = options[:group_ids])
+          notification.send(method)
+        else
+          []
+        end
+      end
+
+      def channel
+        @channel ||= begin
+           value = options[:channel]
+           case value
+           when String
+             value.constantize
+           when Symbol
+             notification.send(value)
+           when Class
+             value
+           else
+             "noticed"
+           end
+         end
+      end
+
+    end
+  end
+end
